039af1d02c3c1747e1db666600b38ddbe628b033
Guyutongxue <guyutongxue@163.com>
2023-03-27 16:22:17 +0800

    add action

--- src/api_def.h
+++ src/api_def.h
@@ -6,23 +6,16 @@
 #define HOST STR(SERVER_HOST)
 #define PORT SERVER_PORT
 
-#define TRANSLATE_PATH "/api/ai/v1/translate"
+#define TRANSLATE_PATH "/api/v1/ai/translate"
 
 #define TRANSLATE_BODY(prompt) \
     nlohmann::json({{"text", prompt}, {"from", "en"}, {"to", "zh"}})
 
-#define CHAT_PATH "/api/ai/v1/chat"
+#define CHAT_PATH "/api/v1/ai/chat"
 
-#define CHAT_BODY(prompt)                     \
-    nlohmann::json({{"prompt", prompt},       \
-                    {"max_tokens", 10},       \
-                    {"temperature", 0.9},     \
-                    {"top_p", 1},             \
-                    {"frequency_penalty", 0}, \
-                    {"presence_penalty", 0},  \
-                    {"stop", {"\n", " Human:", " AI:"}}})
+#define CHAT_BODY(prompt) nlohmann::json({{"prompt", prompt}})
 
-#define DRAW_PATH "/api/ai/v1/draw"
+#define DRAW_PATH "/api/v1/ai/draw"
 
 #define DRAW_BODY(prompt)               \
     nlohmann::json({{"prompt", prompt}, \
@@ -30,6 +23,6 @@
                     {"width", 1024},    \
                     {"height", 1024}})
 
-#define WOLFRAM_PATH "/api/ai/v1/wolfram"
+#define WOLFRAM_PATH "/api/v1/ai/wolfram"
 
 #define WOLFRAM_BODY(prompt) nlohmann::json({{"input", prompt}})
