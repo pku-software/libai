33a9fdacca4281a83ddf47aad71b010f57b1b8b8
Guyutongxue <guyutongxue@163.com>
2023-04-07 12:33:56 +0800

    fix example

--- test/example.cpp
+++ test/example.cpp
@@ -3,13 +3,13 @@
 #include <fstream>
 
 int main() {
-    auto a = ai_create("your token goes here");
-    constexpr int type = AI_TYPE_CHAT;
-    constexpr const char* prompt = "Hi there. Who are you?";
+    auto a = ai_create("5e966ba7-3949-40c1-b207-aa0adab6bda1");
+    constexpr int type = AI_TYPE_DRAW;
+    constexpr const char* prompt = "apple and banana";
     int err = ai_send(a, type, prompt);
     std::cout << err << std::endl;
     int size = ai_result(a, nullptr);
-    char* buf = new char[size + 1];
+    char* buf = new char[size + 1]{};
     ai_result(a, buf);
     if (err || type == AI_TYPE_CHAT || type == AI_TYPE_TRANSLATE) {
         std::cout << buf << std::endl;
